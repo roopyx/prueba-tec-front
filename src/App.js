e4c42baf8d1620692a1c6535539ba92ac5361103
roopyx <roopyx.1111@gmail.com>
2022-10-25 17:53:05 -0300

    send data form to backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import Container from './componentes/Container';
+import Section from './componentes/Section';
+import Input from './componentes/Input';
+import Button from './componentes/Button';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const baseUrl = 'http://127.0.0.1:5000/new/form';
+
+const App = () => {
+  const [form, setForm] = useState({})
+
+  const handleSubmit = ({ expansion, fecha, destino, tonelaje }) => {
+    setForm({ expansion, fecha, destino, tonelaje });
+  }
+
+  const sendForm = async () => {
+    try {
+      const resp = await axios.post(baseUrl, form)
+      console.log(resp);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    sendForm();
+  }, [form]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Section>
+        <Formik
+          initialValues={{
+            expansion: '',
+            fecha: '',
+            destino: '',
+            tonelaje: '',
+          }}
+          onSubmit={handleSubmit}
+          validationSchema={Yup.object({
+            expansion: Yup
+              .string()
+              .required('Campo requerido'),
+            fecha: Yup
+              .date()
+              .required('Campo requerido')
+              .typeError('Debe ser una fecha'),
+            destino: Yup
+              .string()
+              .required('Campo requerido'),
+            tonelaje: Yup
+              .number()
+              .required('Campo requerido')
+              .typeError('Debe ser un numero'),
+            })}>
+          <Form>
+            <Input name='expansion' label='Expansión' />
+            <Input name='fecha' label='Fecha' type='date' />
+            <Input name='destino' label='Destino' />
+            <Input name='tonelaje' label='Tonelaje' />
+            <Button type='submit'>Enviar</Button>
+            <Button type='submit'>Planificación mensual</Button> 
+          </Form>
+        </Formik>
+        {/* {balance !== '' ? <Balance>Balance final: {balance}</Balance> : null} */}
+      </Section>
+    </Container>
+  )
 }
 
-export default App;
+export default App
---
+++ src/componentes/Container.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 100px 100px;
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  align-items: center;
+`
+
+export default Container;
